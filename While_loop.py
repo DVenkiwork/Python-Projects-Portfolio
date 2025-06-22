e45ee0fc9c936f6a1e44d3e560c3dd0771705a08
Venkat Daksh <dakshna.venki@gmail.com>
2025-06-22 00:16:18 -0700

    My first python into remote repo.

---
+++ While_loop.py
@@ -0,0 +1,7 @@
+user_input = int(input("How many time you want to print your name:"))
+loop_count  = 0
+while True:
+    print("Muthu")
+    loop_count = loop_count +1 
+    if loop_count == user_input:
+        break
